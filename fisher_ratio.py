37a12475c7fd5e3538ec16284c9578b31eeb25d9
srvanrell <srvanrell@gmail.com>
2019-11-26 17:26:33 -0300

    Add scaler compatible function. Add some examples

--- fisher_ratio.py
+++ fisher_ratio.py
@@ -1,21 +1,22 @@
 import pandas as pd
 import numpy as np
 
+from sklearn.preprocessing import StandardScaler
+
 
 def fisher_ratio(xy, label_y="group"):
     """xy must be a dataframe with data samples of any size and a column named 'group' (by default).
     Features are given in the columns, and each row corresponds to a sample.
     y column can be named"""
 
-    # Overall statitiscs
+    # Overall statistics
     mean_in_x = xy.mean()       # Gran mean of data samples
     samples_in_x = xy.shape[0]  # Number of data samples
     
-    # Group statitsitcs
+    # Group statistics
     mean_in_group = xy.groupby(label_y).mean()      # group means
     samples_in_group = xy.groupby(label_y).size()   # group sizes
     number_of_groups = xy.groupby(label_y).ngroups  # number of groups
-
 
     # Computing between-group variability
     diff_group_mean_gran_mean = mean_in_group - mean_in_x
@@ -23,7 +24,6 @@ def fisher_ratio(xy, label_y="group"):
     numerator = np.sum([samples_in_group.loc[group_label] *
                         np.dot(diff_group_mean_gran_mean.loc[group_label], diff_group_mean_gran_mean.loc[group_label])
                         for group_label in diff_group_mean_gran_mean.index]) / (number_of_groups - 1)
-
 
     # Computing within-group variability
     xy_grouped = xy.groupby(label_y).groups  # dictionary with group-labels as key and group-indexes as values
@@ -40,3 +40,18 @@ def fisher_ratio(xy, label_y="group"):
     ratio = 1.0 * numerator / denominator
 
     return ratio
+
+
+def scale_xy(xy, label_y="group"):
+    """Apply sklearn StandardScaler to features columns"""
+    y = xy.loc[:, label_y]
+    xy_dropped = xy.drop(label_y, axis="columns")  # Drop group column before scaling
+
+    scaler = StandardScaler()
+    scaler.fit(xy_dropped)
+
+    # Scaled data and restore group column
+    new_xy = pd.DataFrame(scaler.transform(xy_dropped), columns=xy_dropped.columns)
+    new_xy.loc[:, label_y] = y
+
+    return new_xy
--- test_fisher_ratio.py
+++ test_fisher_ratio.py
@@ -1,4 +1,4 @@
-from fisher_ratio import fisher_ratio
+from fisher_ratio import fisher_ratio, scale_xy
 from sklearn.preprocessing import StandardScaler
 import numpy as np
 import pandas as pd
@@ -13,16 +13,42 @@ df = pd.DataFrame({"dim1": 1.0*aux_array,
                    "group": aux_groups})
 
 print("\nunscaled data\n", df)
+print("\nfisher ratio unscaled:", fisher_ratio(df))
 
-df = df.drop("group", axis="columns")  # Drop group column before scaling
-
-scaler = StandardScaler()
-scaler.fit(df)
-
-# Scaled data and restore group column
-new_df = pd.DataFrame(scaler.transform(df), columns=df.columns)
-new_df.loc[:, "group"] = aux_groups
+new_df = scale_xy(df, label_y="group")
 
 print("\nscaled data\n", new_df)
+print("\nfisher ratio scaled:", fisher_ratio(new_df))
 
-print("\nfisher ratio:", fisher_ratio(new_df))
+
+aux_array2 = np.concatenate([np.arange(0, 3), np.arange(100, 103), np.arange(10, 13)])
+aux_groups2 = ["a"]*3 + ["b"]*3 + ["c"]*3
+
+df2 = pd.DataFrame({"dim1": 1.0*aux_array2,
+                    "dim2": 1.0*aux_array2,
+                    "group": aux_groups2})
+
+print("\nunscaled data\n", df2)
+print("\nfisher ratio unscaled:", fisher_ratio(df2))
+
+new_df2 = scale_xy(df2, label_y="group")
+
+print("\nscaled data\n", new_df2)
+print("\nfisher ratio scaled:", fisher_ratio(new_df2))
+
+
+aux_array3 = np.concatenate([np.arange(0, 3), np.arange(0, 3), np.arange(0, 3)])
+aux_groups3 = ["a"]*3 + ["b"]*3 + ["c"]*3
+
+
+df3 = pd.DataFrame({"dim1": 1.0*aux_array3,
+                    "dim2": 1.0*aux_array3,
+                    "group": aux_groups3})
+
+print("\nunscaled data\n", df3)
+print("\nfisher ratio unscaled:", fisher_ratio(df3))
+
+new_df3 = scale_xy(df3, label_y="group")
+
+print("\nscaled data\n", new_df3)
+print("\nfisher ratio scaled:", fisher_ratio(new_df3))
